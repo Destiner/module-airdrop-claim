46a3e65de43a97f6602f37532f8284468e256ab3
Timur Badretdinov <accounts@destiner.io>
2024-03-09 10:05:45 +0000

    Init token creation script

--- src/claim-sim/scripts/create-token.ts
+++ src/claim-sim/scripts/create-token.ts
@@ -1,1 +1,15 @@
-import {} from "viem";
+import { getContractAddress } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+
+import { privateKey, salt } from "../utils/env";
+
+const account = privateKeyToAccount(privateKey);
+
+const address = getContractAddress({
+  opcode: "CREATE2",
+  salt: salt,
+  bytecode: "0x",
+  from: account.address,
+});
+
+console.log(`Deploying token at: ${address}`);
