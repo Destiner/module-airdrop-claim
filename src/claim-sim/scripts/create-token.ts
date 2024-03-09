1118e2a4305c15a2e72e6f67335eace0c463b846
Timur Badretdinov <accounts@destiner.io>
2024-03-09 10:46:52 +0000

    Tweak token creation script

--- src/claim-sim/scripts/create-token.ts
+++ src/claim-sim/scripts/create-token.ts
@@ -1,15 +1,39 @@
-import { getContractAddress } from "viem";
+import { getContractAddress, createWalletClient, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
+import { polygonMumbai } from "viem/chains";
 
 import { privateKey, salt } from "../utils/env";
+import { token as tokenBytecode } from "../utils/bytecode";
+import erc20Abi from "../abi/erc20";
 
-const account = privateKeyToAccount(privateKey);
+async function main() {
+  const account = privateKeyToAccount(privateKey);
 
-const address = getContractAddress({
-  opcode: "CREATE2",
-  salt: salt,
-  bytecode: "0x",
-  from: account.address,
-});
+  const walletClient = createWalletClient({
+    chain: polygonMumbai,
+    transport: http(),
+    account,
+  });
 
-console.log(`Deploying token at: ${address}`);
+  // walletClient.switchChain({ id: polygonMumbai.id });
+
+  const hash = await walletClient.deployContract({
+    abi: erc20Abi,
+    account,
+    bytecode: tokenBytecode,
+    args: ["Token", "TKN1", account.address],
+  });
+
+  console.log(`Deploying token at: ${hash}`);
+
+  const address = getContractAddress({
+    opcode: "CREATE2",
+    salt: salt,
+    bytecode: tokenBytecode,
+    from: account.address,
+  });
+
+  console.log(`Deploying token at: ${address}`);
+}
+
+main();
